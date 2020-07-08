e2065ff01b4430e9e755ffa1b209008b8d85fafc
Mike Evlantev <mikeev21@gmail.com>
2020-07-08 12:59:15 -0700

    Caliburn Micro DI setup

--- Retailer.Desktop.UI/Bootstrapper.cs
+++ Retailer.Desktop.UI/Bootstrapper.cs
@@ -3,6 +3,7 @@ using Retailer.Desktop.UI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,14 +12,40 @@ namespace Retailer.Desktop.UI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // DI: handles instantiation of classes
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(vmType => _container.RegisterPerRequest(
+                    vmType, 
+                    vmType.ToString(), 
+                    vmType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
         }
+
+        protected override object GetInstance(Type service, string key) => _container.GetInstance(service, key);
+
+        protected override IEnumerable<object> GetAllInstances(Type service) => _container.GetAllInstances(service);
+
+        protected override void BuildUp(object instance) => _container.BuildUp(instance);
     }
 }
