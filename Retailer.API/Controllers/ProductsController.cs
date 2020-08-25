aaf90c764878610f272a64115c086cb82e74e9a3
Mike Evlantev <mikeev21@gmail.com>
2020-08-25 13:03:24 -0700

    Products endpoints set up

---
+++ Retailer.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Retailer.Core.Models;
+using Retailer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Retailer.API.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/products")]
+    public class ProductsController : ApiController
+    {
+        //[Route("getall")]
+        public async Task<IEnumerable<IProductModel>> GetAllProducts()
+        {
+            var repo = new ProductRepository();
+            return await repo.GetAllProducts();
+        }
+    }
+}
