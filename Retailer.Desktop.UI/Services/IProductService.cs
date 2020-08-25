51a4da1f51e72c5decdbff73975790990993c0ce
Mike Evlantev <mikeev21@gmail.com>
2020-08-25 14:23:53 -0700

    ui service setup to call products api controller

---
+++ Retailer.Desktop.UI/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Retailer.Desktop.UI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Retailer.Desktop.UI.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProducts();
+    }
+}
---
+++ Retailer.Desktop.UI/Services/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Retailer.Core.Helpers;
+using Retailer.Desktop.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailer.Desktop.UI.Services
+{
+    public class ProductService : IProductService
+    {
+        private IApiHelper _apiHelper;
+
+        public ProductService(IApiHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllProducts()
+        {
+            using (HttpResponseMessage response = await _apiHelper.ApiClient.GetAsync("/api/Products"))
+            {
+                if (response.IsSuccessStatusCode)
+                    return await response.Content.ReadAsAsync<IEnumerable<ProductModel>>();
+                else
+                    throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
