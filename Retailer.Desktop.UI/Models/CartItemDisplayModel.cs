b993898568964e3555ae2708450119410232839d
Mike Evlantev <mikeev21@gmail.com>
2020-09-10 14:03:07 -0700

    Display models for UI to be mapped with AutoMapper

---
+++ Retailer.Desktop.UI/Models/CartItemDisplayModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retailer.Desktop.UI.Models
+{
+    public class CartItemDisplayModel : INotifyPropertyChanged
+    {
+        private int _quantity;
+
+        public ProductDisplayModel Product { get; set; }
+              
+        public int Quantity
+        {
+            get { return _quantity; }
+            set 
+            { 
+                _quantity = value;
+                CallPropertyChanged(nameof(Quantity));
+                CallPropertyChanged(nameof(DisplayText));
+            }
+        }
+
+        public string DisplayText
+        {
+            get
+            {
+                return $"{Product.Name} ({Quantity})";
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CallPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Retailer.Desktop.UI/Models/ProductDisplayModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Retailer.Desktop.UI.Models
+{
+    public class ProductDisplayModel : INotifyPropertyChanged
+    {
+        private int _inStock;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double RetailPrice { get; set; }
+
+        public int InStock
+    {
+            get { return _inStock; }
+            set 
+            { 
+                _inStock = value;
+                CallPropertyChanged(nameof(InStock));
+            }
+        }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public bool IsTaxable { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void CallPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
