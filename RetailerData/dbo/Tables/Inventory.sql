cd6b0c527b88c1d942066e3381a62d8fcb93d14e
Mike Evlantev <mikeev21@gmail.com>
2020-09-11 12:05:27 -0700

    allows the user to define purchase date

--- RetailerData/dbo/Tables/Inventory.sql
+++ RetailerData/dbo/Tables/Inventory.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Inventory]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [ProductId] INT NOT NULL, 
+    [ProductId] INT NOT NULL,
     [Quantity] INT NOT NULL, 
     [PurchasePrice] MONEY NOT NULL, 
-    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    [PurchaseDate] DATETIME2 NOT NULL, 
     CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id]) 
 )
